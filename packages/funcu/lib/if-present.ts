9f2ee07f55b2e1341eae660ceb04dec9cbe3cf02
siyual-park <siyual.bak@gmail.com>
2020-10-19 10:43:58 +0900

    fix null check

--- packages/funcu/lib/if-present.ts
+++ packages/funcu/lib/if-present.ts
@@ -1,13 +1,19 @@
-function ifPresent<T, R>(receiver: T, block: (receiver: T) => R): R;
-function ifPresent<T, R>(receiver: null, block: (receiver: T) => R): null;
+function ifPresent<T, R>(
+  receiver: T,
+  block: (receiver: Exclude<T, null & undefined>) => R
+): R;
+function ifPresent<T, R>(
+  receiver: null,
+  block: (receiver: Exclude<T, null & undefined>) => R
+): null;
 function ifPresent<T, R>(
   receiver: undefined,
-  block: (receiver: T) => R
+  block: (receiver: Exclude<T, null & undefined>) => R
 ): undefined;
 
 function ifPresent<T, R>(
   receiver: T | null | undefined,
-  block: (receiver: T) => R
+  block: (receiver: Exclude<T, null & undefined>) => R
 ): R | null | undefined {
   if (receiver === null) return null;
   if (receiver === undefined) return undefined;
